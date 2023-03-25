aa2d09aff2d893328e69bd66d6de0c5a6bd29b45
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-03-25 20:03:39 +0600

    Google Sign-In

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,10 +1,10 @@
-import 'dart:math';
+
+import 'dart:developer';
 
 import 'package:chatting_app/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
 import '../../main.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -17,6 +17,35 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
 
 
+  //Firebase Google email authentication code
+  _handleGoogleBtnClick() {
+
+    _signInWithGoogle().then((user) {
+      log("\nUser : ${user.user}");
+      log("\nUserAdditionalInfo: ${user.additionalUserInfo}");
+    
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+    });
+  }
+
+  Future<UserCredential> _signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +60,6 @@ class _LoginScreenState extends State<LoginScreen> {
       //body
       body: Stack(
         children: [
-
           //App logo icon
           Positioned(
             top: mq.height * .15,
@@ -40,7 +68,6 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Image.asset(
               "images/icon.png",
             ),
-
           ),
 
           //google login button
@@ -85,31 +112,4 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-
-  //Firebase Google email authentication code
-  _handleGoogleBtnClick() {
-    _signInWithGoogle().then((user) {
-
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> const HomeScreen()));
-    });
-  }
-
-  Future<UserCredential> _signInWithGoogle() async {
-    
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-
-    // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance.signInWithCredential(credential);
-  }
-
 }
